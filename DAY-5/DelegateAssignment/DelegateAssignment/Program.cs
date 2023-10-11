f5da8e7dff25eed6bee87c79cd2f4a04309cefa4
Kunal Chandel <kunalchandel266@gmail.com>
2023-10-11 22:18:56 +0530

    day-5

---
+++ DAY-5/DelegateAssignment/DelegateAssignment/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate int MyDelegate(int num);
+
+namespace DelegateAssignment
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+
+            Main mainInstance = new Main();
+            Cube cubeInstance = new Cube();
+
+            // Subscribe to the event
+            mainInstance.OnEvent += cubeInstance.cubeCal;
+
+            // Get input from the user
+            Console.Write("Enter a number to calculate its cube: ");
+            if (int.TryParse(Console.ReadLine(), out int userInput))
+            {
+                // Raise the event and get the result
+                int cubeResult = mainInstance.raiseEvent(userInput);
+                Console.WriteLine("Cube of {0} is: {1}", userInput, cubeResult);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid Number.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class Main
+    {
+        public event MyDelegate OnEvent;
+
+        public int raiseEvent(int num)
+        {
+           return  OnEvent?.Invoke(num) ?? 0;
+        }
+    }
+
+    class Cube
+    {
+        public int cubeCal(int num)
+        {
+            int result = num*num*num;
+            return result;
+        }
+    }
+}
---
+++ DAY-5/LinqAssignment/LinqAssignment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a collection of students
+            List<Student> students = new List<Student>
+        {
+            new Student { Name = "Alice", Age = 20, GPA = 3.5 },
+            new Student { Name = "Bob", Age = 22, GPA = 3.2 },
+            new Student { Name = "Charlie", Age = 19, GPA = 3.8 },
+            // Add more students as needed
+        };
+
+            // LINQ query to filter students with GPA greater than 3.5
+            var highGpaStudents = students.Where(s => s.GPA > 3.5);
+
+            // LINQ query to order students by GPA in descending order
+            var orderedStudents = students.OrderByDescending(s => s.GPA);
+
+            // Display the results
+            Console.WriteLine("High GPA Students:");
+            foreach (var student in highGpaStudents)
+            {
+                Console.WriteLine($"{student.Name} - Age: {student.Age}, GPA: {student.GPA}");
+            }
+
+            Console.WriteLine("\nOrdered Students by GPA:");
+            foreach (var student in orderedStudents)
+            {
+                Console.WriteLine($"{student.Name} - Age: {student.Age}, GPA: {student.GPA}");
+            }
+
+            // Example of data manipulation - calculating the average GPA
+            double averageGpa = students.Average(s => s.GPA);
+            Console.WriteLine($"\nAverage GPA: {averageGpa}");
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double GPA { get; set; }
+    }
+
+}
